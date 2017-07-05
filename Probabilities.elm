3e139960275e85f65f8d9c874c16a80ad66ce0d3
Jan Christiansen <jan.christiansen@hs-flensburg.de>
2017-07-05 22:20:49 +0200

    Move special casing to probabilities

--- Probabilities.elm
+++ Probabilities.elm
@@ -8,29 +8,27 @@ import Random exposing (Generator)
 
 --
 probs : List (Dict Int Float) -> QOBDD -> List Float
-probs probs qobdd =
-  List.indexedMap (\player probs -> prob player probs qobdd) probs
+probs probs qobdd = List.map (\probs -> prob probs qobdd) probs
 
--- The power of a specific player given specific probabilities
-prob : Int -> Dict Int Float -> QOBDD -> Float
-prob player probs qobdd = probTree player probs qobdd.root
+prob : Dict Int Float -> QOBDD -> Float
+prob probs qobdd = probTree probs qobdd.root
 
--- The player accepts her own proposal with probability 1
-probTree : Int -> Dict Int Float -> Tree -> Float
-probTree player probs =
+probTree : Dict Int Float -> Tree -> Float
+probTree probs =
   let f label p1 p2 =
-    if label == player
-    then p1
-    else
-      case get label probs of
-        Nothing -> Debug.crash ("Label " ++ toString label ++ " not found in "  ++ toString probs)
-        Just p -> p * p1 + (1-p) * p2
+    case get label probs of
+      Nothing -> Debug.crash ("Label " ++ toString label ++ " not found in "  ++ toString probs)
+      Just p -> p * p1 + (1-p) * p2
   in
   foldTreeShare 0 1 f
 
 
+-- the diagonal is one
 halves : Int -> List (List Float)
-halves n = List.repeat n (List.repeat n 0.5)
+halves n = List.indexedMap halvesRow (List.repeat n n)
+
+halvesRow : Int -> Int -> List Float
+halvesRow i n = List.repeat i 0.5 ++ 1 :: List.repeat (n-i-1) 0.5
 
 probsGenerator : Int -> Generator (List (List Float))
 probsGenerator n =

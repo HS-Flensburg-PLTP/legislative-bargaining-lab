1d13b5552cd5524c957aa8de4e6b05b4c7953592
Jan Christiansen <jan.christiansen@hs-flensburg.de>
2017-09-08 13:42:05 +0200

    Use normal list for vector

--- Vector.elm
+++ Vector.elm
@@ -1,9 +1,8 @@
 module Vector
     exposing
-        ( Vector(..)
+        ( Vector
         , extend
         , fromList
-        , map
         , minus
         , mult
         , one
@@ -13,63 +12,36 @@ module Vector
         )
 
 
-fromList : List a -> Vector a
-fromList xs =
-    case xs of
-        [] ->
-            Debug.crash "empty list"
-
-        [ x ] ->
-            Single x
-
-        y :: ys ->
-            More y (fromList ys)
-
-
 toList : Vector a -> List a
-toList v =
-    case v of
-        Single x ->
-            [ x ]
-
-        More x v_ ->
-            x :: toList v_
+toList =
+    identity
 
 
-type Vector a
-    = Single a
-    | More a (Vector a)
+fromList : List a -> Vector a
+fromList =
+    identity
 
 
-map : (a -> b) -> Vector a -> Vector b
-map f xs =
-    case xs of
-        Single a ->
-            Single (f a)
-
-        More a aVector ->
-            More (f a) (map f aVector)
+type alias Vector a =
+    List a
 
 
 map2 : (a -> a -> a) -> Vector a -> Vector a -> Vector a
-map2 f xs ys =
-    case ( xs, ys ) of
-        ( Single v1, Single v2 ) ->
-            Single (f v1 v2)
+map2 f v1 v2 =
+    case ( v1, v2 ) of
+        ( [], _ ) ->
+            v2
 
-        ( Single v1, More v2 vt2_ ) ->
-            More (f v1 v2) vt2_
+        ( _, [] ) ->
+            v1
 
-        ( More v1 vt1_, Single v2 ) ->
-            More (f v1 v2) vt1_
-
-        ( More v1 vt1_, More v2 vt2_ ) ->
-            More (f v1 v2) (map2 f vt1_ vt2_)
+        ( a1 :: v12, a2 :: v22 ) ->
+            f a1 a2 :: map2 f v12 v22
 
 
 zero : Vector Int
 zero =
-    Single 0
+    []
 
 
 plus : Vector Int -> Vector Int -> Vector Int
@@ -79,35 +51,25 @@ plus =
 
 one : Vector Int
 one =
-    Single 1
-
-
-mult_ : Vector Int -> Vector Int -> Vector Int
-mult_ vt1 vt2 =
-    case vt1 of
-        Single v1 ->
-            map ((*) v1) vt2
-
-        More v1 vt1_ ->
-            plus (map ((*) v1) vt2) (More 0 (mult_ vt1_ vt2))
+    [ 1 ]
 
 
 mult : Vector Int -> Vector Int -> Vector Int
-mult vt1 vt2 =
-    case ( vt1, vt2 ) of
-        ( Single 0, _ ) ->
-            Single 0
+mult v1 v2 =
+    case ( v1, v2 ) of
+        ( [], _ ) ->
+            []
 
-        ( _, Single 0 ) ->
-            Single 0
+        ( _, [] ) ->
+            []
 
-        ( _, _ ) ->
-            mult_ vt1 vt2
+        ( n :: v12, _ ) ->
+            plus (List.map ((*) n) v2) (0 :: mult v12 v2)
 
 
 extend : Vector Int -> Vector Int
 extend v =
-    More 0 v
+    0 :: v
 
 
 minus : Vector Int -> Vector Int -> Vector Int

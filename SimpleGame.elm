794f30dc565cf9ffec86dd1baca674740af0fa94
Frederik Hamester <frederik.hamester@stud.hs-flensburg.de>
2018-06-04 14:56:54 +0200

    Add type for JoinTree

--- SimpleGame.elm
+++ SimpleGame.elm
@@ -6,14 +6,42 @@ import List exposing (..)
 import Result exposing (..)
 
 
+type JoinTree
+    = BoolEmpty
+    | BoolValue Int
+    | BoolAnd JoinTree JoinTree
+    | BoolOr JoinTree JoinTree
+
+
+type alias Player =
+    { m : Int, name : String, id : Int }
+
+
 type alias RuleMVG =
     { quota : Int, weights : List Int }
 
 
 type alias SimpleGame =
-    { n : Int, rules : List RuleMVG, ruleCount : Int }
+    { n : Int, rules : List RuleMVG, ruleCount : Int, players : List Player, joinTree : Json.Value }
 
 
+joinTreeDecoder : Json.Decoder Json.Value
+joinTreeDecoder =
+    Json.value
+
+
+{-| decoder for js player
+-}
+playerDecoder : Json.Decoder Player
+playerDecoder =
+    Json.map3 Player
+        (Json.field "m" Json.int)
+        (Json.field "name" (Json.oneOf [ Json.string, Json.succeed "" ]))
+        (Json.field "id" Json.int)
+
+
+{-| decoder for js rule objects
+-}
 ruleDecoder : Json.Decoder RuleMVG
 ruleDecoder =
     Json.map2 RuleMVG
@@ -21,27 +49,37 @@ ruleDecoder =
         (Json.field "weights" (Json.list (Json.oneOf [ Json.int, Json.succeed 0 ])))
 
 
+{-| like take just reversed
+-}
 takeReverse : Int -> List a -> List a
 takeReverse n l =
     reverse (take n (reverse l))
 
 
+{-| removes artifacts coursed by undefined objects in js lists
+-}
 cleanRules : Int -> RuleMVG -> RuleMVG
 cleanRules n rule =
     { rule | weights = takeReverse n rule.weights }
 
 
+{-| removes artifacts coursed by undefined objects in js lists
+-}
 cleanSimpleGame : SimpleGame -> SimpleGame
 cleanSimpleGame game =
     { game | rules = List.map (\rule -> cleanRules game.n rule) (takeReverse game.ruleCount game.rules) }
 
 
+{-| decodes Json to Elm Simple Game
+-}
 simpleGameDecoder : Json.Decoder SimpleGame
 simpleGameDecoder =
-    Json.map3 SimpleGame
+    Json.map5 SimpleGame
         (Json.field "n" Json.int)
         (Json.field "rules" (Json.list (Json.oneOf [ ruleDecoder, Json.succeed (RuleMVG 0 []) ])))
         (Json.field "ruleCount" Json.int)
+        (Json.field "classes" (Json.list playerDecoder))
+        (Json.field "joinTree" joinTreeDecoder)
 
 
 port parseSimpleGame : String -> Cmd msg
@@ -50,6 +88,8 @@ port parseSimpleGame : String -> Cmd msg
 port parsedSimpleGameJson : (Json.Encode.Value -> msg) -> Sub msg
 
 
+{-| decodes js simple games object as elm object and produces massage with the elm object
+-}
 parsedSimpleGame : (SimpleGame -> msg) -> Sub msg
 parsedSimpleGame f =
     parsedSimpleGameJson

ed92a4df6e3c49a6baac9e92ce6ce0eaf6ead4be
Frederik Hamester <frederik.hamester@stud.hs-flensburg.de>
2018-08-06 16:23:20 +0200

    Add first attempted to add sharing

--- QOBDDBuilders.elm
+++ QOBDDBuilders.elm
@@ -1,5 +1,6 @@
 module QOBDDBuilders exposing (fromSGToSimpleQOBDD)
 
+import Dict exposing (Dict)
 import QOBDD exposing (..)
 import SimpleGame exposing (..)
 import Tuple exposing (first)
@@ -8,24 +9,55 @@ import Tuple exposing (first)
 {-| Takes a Node id a quota and a list of player weights
 and cursively calls itself to generate a BDD.
 -}
-buildBDDWithIds : NodeId -> Quota -> List PlayerWeight -> List Player -> ( BDD, NodeId )
-buildBDDWithIds id quota weights players =
+buildBDDWithIds : NodeId -> Quota -> List PlayerWeight -> List Player -> Dict ( ( NodeId, Int ), PlayerId, ( NodeId, Int ) ) NodeId -> ( BDD, ( NodeId, Dict ( ( NodeId, Int ), PlayerId, ( NodeId, Int ) ) NodeId ) )
+buildBDDWithIds id quota weights players dict1 =
     case ( weights, players ) of
         ( w :: ws, p :: ps ) ->
             let
-                ( lTree, lTreeId ) =
-                    buildBDDWithIds id (quota - w) ws ps
+                ( lTree, ( lTreeId, dict2 ) ) =
+                    buildBDDWithIds id (quota - w) ws ps dict1
 
-                ( rTree, rTreeId ) =
-                    buildBDDWithIds lTreeId quota ws ps
+                ( rTree, ( rTreeId, dict3 ) ) =
+                    buildBDDWithIds lTreeId quota ws ps dict2
+
+                nodeInfo =
+                    ( case lTree of
+                        Zero ->
+                            ( 0, 0 )
+
+                        One ->
+                            ( 0, 1 )
+
+                        _ ->
+                            ( lTreeId, 2 )
+                    , p.id
+                    , case lTree of
+                        Zero ->
+                            ( 0, 0 )
+
+                        One ->
+                            ( 0, 1 )
+
+                        _ ->
+                            ( rTreeId, 2 )
+                    )
             in
-            ( Node { id = rTreeId, thenB = lTree, var = p.id, elseB = rTree }, rTreeId + 1 )
+            ( case Dict.get nodeInfo dict3 of
+                Just nodeId ->
+                    Ref nodeId
+
+                Nothing ->
+                    Node { id = rTreeId, thenB = lTree, var = p.id, elseB = rTree }
+            , ( rTreeId + 1
+              , Dict.insert nodeInfo rTreeId dict3
+              )
+            )
 
         ( _, _ ) ->
             if quota > 0 then
-                ( Zero, id )
+                ( Zero, ( id, dict1 ) )
             else
-                ( One, id )
+                ( One, ( id, dict1 ) )
 
 
 {-| Takes a SimpleGame and generates a QOBDD
@@ -40,5 +72,5 @@ fromSGToSimpleQOBDD game =
                 Zero
 
             rule :: rules ->
-                first (buildBDDWithIds 0 rule.quota rule.weights game.players)
+                first (buildBDDWithIds 0 rule.quota rule.weights game.players Dict.empty)
         )
